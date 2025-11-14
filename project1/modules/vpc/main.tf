5938927fe635c42794badb389e0e6e5c976b76dd
vanaraj <raaju181193@gmail.com>
2025-11-14 16:09:08 +0530

    added routes

--- project1/modules/vpc/main.tf
+++ project1/modules/vpc/main.tf
@@ -16,3 +16,35 @@ resource "aws_subnet" "sb" {
     }
   
 }
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.vpc.id
+    tags = {
+        Name = "${var.vpc_name}-igw"
+    }
+  
+}
+
+resource "aws_route_table" "rt" {
+    vpc_id = aws_vpc.vpc.id
+    route  {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id 
+    }
+    tags = {
+        Name = "${var.vpc_name}-rt"
+    }
+}
+
+resource "aws_route" "route" {
+    route_table_id         = aws_route_table.rt.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id             = aws_internet_gateway.igw.id
+  
+}
+
+resource "aws_route_table_association" "a" {
+  count = length(var.subnet_cidr)
+  subnet_id      = aws_subnet.sb[count.index].id
+  route_table_id = aws_route_table.rt.id
+}
