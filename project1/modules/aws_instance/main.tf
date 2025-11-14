04f527b0b904e7a0e0abf1004163a55897735666
vanaraj <raaju181193@gmail.com>
2025-11-14 17:36:47 +0530

    fixed vpc_id in tg | main.tf instance

--- project1/modules/aws_instance/main.tf
+++ project1/modules/aws_instance/main.tf
@@ -36,11 +36,11 @@ resource "aws_security_group" "allow_ssh" {
   }
 }   
 
-resource "aws_lb_target_group" "tcp-example" {
+resource "aws_lb_target_group" "tg" {
   name     = "tf-test-tg-tcp"
   port     = 80
   protocol = "TCP"
-  vpc_id   = module.vpc.vpc_main_id
+  vpc_id   = var.vpc_main_id
 
   target_group_health {
     dns_failover {
@@ -59,7 +59,7 @@ resource "aws_lb_target_group_attachment" "test" {
   for_each = {
     for k, v in aaws_instance.web1 : k => v
   }
-  target_group_arn = aws_lb_target_group.tcp-example.arn
+  target_group_arn = aws_lb_target_group.tg.arn
   target_id        = each.value.id
   port             = 80
 }
