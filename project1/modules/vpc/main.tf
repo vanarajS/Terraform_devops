b95e81e0e851f38e06b5deb3a161d4f995f3e100
vanaraj <raaju181193@gmail.com>
2025-11-14 16:23:25 +0530

    Changed duplicat route

--- project1/modules/vpc/main.tf
+++ project1/modules/vpc/main.tf
@@ -36,14 +36,8 @@ resource "aws_route_table" "rt" {
     }
 }
 
-resource "aws_route" "route" {
-    route_table_id         = aws_route_table.rt.id
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id             = aws_internet_gateway.igw.id
-  
-}
 
-resource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "route_assoc" {
   count = length(var.subnet_cidr)
   subnet_id      = aws_subnet.sb[count.index].id
   route_table_id = aws_route_table.rt.id
