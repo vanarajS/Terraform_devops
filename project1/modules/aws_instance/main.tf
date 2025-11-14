77d45e65192a328f3239f9faf74eb35c6e73100d
vanaraj <raaju181193@gmail.com>
2025-11-14 17:50:42 +0530

    added lb

--- project1/modules/aws_instance/main.tf
+++ project1/modules/aws_instance/main.tf
@@ -64,4 +64,49 @@ resource "aws_lb_target_group_attachment" "test" {
   port             = 80
 }
 
+resource "aws_security_group" "lb-sg" {
+  name        = "allow_lb"
+  description = "Allow lb inbound traffic"
+  vpc_id      = var.vpc_main_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+}
+
+resource "aws_lb" "lb" {
+  name               = "test-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb-sg.id]
+  subnets            = [for subnet in var.subnet_id : subnet]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
 
