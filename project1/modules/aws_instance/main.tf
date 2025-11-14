817f86a6a69412c2cf38fb1a794bf537cadd91b4
vanaraj <raaju181193@gmail.com>
2025-11-14 16:56:17 +0530

    changed

--- project1/modules/aws_instance/main.tf
+++ project1/modules/aws_instance/main.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
+  key_name   = "deployer-key[${timestamp()}]"
   public_key = file(var.public_key_path)
   }
 
@@ -9,4 +9,27 @@ resource "aws_instance" "web1" {
   key_name = aws_key_pair.deployer.key_name
   subnet_id = var.subnet_id[count.index]
   ami = var.os_name
+
+  tags = {
+    Name = "web-${count.index}"
+  }
 }
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = element(var.subnet_id, 0) != null ? aws_subnet.sb[0].vpc_id : null
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}   
