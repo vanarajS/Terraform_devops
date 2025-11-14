ba146970c133ed4da74f8e9e5bc00287f7ef91ba
vanaraj <raaju181193@gmail.com>
2025-11-14 17:19:04 +0530

    enable public ip

--- project1/modules/aws_instance/main.tf
+++ project1/modules/aws_instance/main.tf
@@ -9,8 +9,7 @@ resource "aws_instance" "web1" {
   key_name = aws_key_pair.deployer.key_name
   subnet_id = var.subnet_id[count.index]
   ami = var.os_name
-  public_ip = true
-  public_dns = true
+  associate_public_ip_address = true 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
