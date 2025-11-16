cf5e94905ebd4140c0e0833b28384e4d2596360a
vanaraj <raaju181193@gmail.com>
2025-11-16 19:53:41 +0530

    changed dir

--- project1/main.tf
+++ project1/main.tf
@@ -13,11 +13,11 @@ module "vpc" {
 
 module "instance" {
     source = "./modules/aws_instance"
-    public_key_path    = "./ssh-key"
+    public_key_path    = "./project1/ssh-key"
     os_name            = "ami-069e612f612be3a2b"
     aws_instance_count = 2
     instance_type      = "t2.micro"
     subnet_id          = module.vpc.subnet_ids
     vpc_main_id        = module.vpc.vpc_main_id
-    app-testing        = "./user-data.sh"
+    app-testing        = "./project1/user-data.sh"
 }
