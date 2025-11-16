e0a446108b119b04f29f13ddc392f595e52df49d
vanaraj <raaju181193@gmail.com>
2025-11-16 20:13:07 +0530

    c

--- project1/main.tf
+++ project1/main.tf
@@ -13,11 +13,11 @@ module "vpc" {
 
 module "instance" {
     source = "./modules/aws_instance"
-    public_key_path    = file("${path.root}/user-data.sh")
+    public_key_path    = file(ssh-key)
     os_name            = "ami-069e612f612be3a2b"
     aws_instance_count = 2
     instance_type      = "t2.micro"
     subnet_id          = module.vpc.subnet_ids
     vpc_main_id        = module.vpc.vpc_main_id
-    app-testing        = file("${path.root}/user-data.sh")
+    app-testing        = file(user_data.sh)
 }
