32e215591dff452859818863936b7438255a615a
vanaraj <raaju181193@gmail.com>
2025-11-16 19:41:59 +0530

    added key

--- project1/main.tf
+++ project1/main.tf
@@ -13,7 +13,7 @@ module "vpc" {
 
 module "instance" {
     source = "./modules/aws_instance"
-    public_key_path    = "~/.ssh/id_rsa.pub"
+    public_key_path    = "ssh-key"
     os_name            = "ami-069e612f612be3a2b"
     aws_instance_count = 2
     instance_type      = "t2.micro"
